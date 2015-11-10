3cc4d2a6667a2a76f7c20950c575f39663137d09
slimypit <david@aiken5.com>
2015-11-09 18:37:29 -0800

    Docker Demo for container talk

---
+++ hello2/src/server.js
@@ -0,0 +1,18 @@
+// Load the http module to create an http server.
+var http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World 2\n");
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+var port = process.env.PORT || 8080;
+var host = process.env.VCAP_APP_HOST || "127.0.0.1";
+console.log("Starting Server running at " + host + ":" + port );
+
+server.listen(port);
+
+// Put a friendly message on the terminal
+console.log("Server running at " + host + ":" + port );
